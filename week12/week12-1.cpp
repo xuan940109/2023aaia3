e32c112ac76a22c456dc8ab216b81c5cf49975a7
xuan940109 <144758276+xuan940109@users.noreply.github.com>
2024-11-28 16:19:10 +0800

    week12

---
+++ week12/week12-1.cpp
@@ -0,0 +1,19 @@
+///week12-1.cpp
+#include <iostream>
+using namespace std;
+///struct Node {};
+///class Cat {};
+struct Node {
+    int Val;
+};
+class Cat {
+public:
+    string name;
+};
+int main()
+{
+    Cat cat1;
+    cat1.name = "Kitty";
+    Node node;
+    node.val = 1;
+}
