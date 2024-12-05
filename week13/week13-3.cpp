283248af7a58874e2f0be565bc9bfa0b45949d26
xuan940109 <144758276+xuan940109@users.noreply.github.com>
2024-12-05 16:34:28 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,13 @@
+//week13-3.cpp
+
+int main() {
+    vector<int> A,B;
+    int a,b;
+    while(cin>>a>>b){
+        A.push_back(a);
+        B.push_back(b);
+    }
+    for(int i=0;i<A.size();i++){
+        cout << A[i] << " ";
+    }
+}
