57bc11c4aba888bcb1ad0e0d58d10ac2563280b5
xuan940109 <144758276+xuan940109@users.noreply.github.com>
2024-12-26 13:22:28 +0800

    week15

---
+++ week15/weekl15-1.cpp
@@ -0,0 +1,16 @@
+//week15-1.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string hello;
+	int t=1;
+	while(cin>>hello)
+	{
+		if(hello=="#") break;
+
+		cout<<"Case "<<t<<": ENGLISH\n";
+		t++;
+	}
+}
